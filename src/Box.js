3704119417d96bdc3ec7b49f25c2944f241c7a35
Andres Mena <ballerlikemike23@gmail.com>
2019-06-26 16:51:52 -0400

    Functionality for picking a random color

--- src/Box.js
+++ src/Box.js
@@ -1,22 +1,35 @@
 import React, { Component } from 'react'
+import { choice } from "./helper"
+import './Box.css'
 
 class Box extends Component {
+  static defaultProps = {
+    allColors: ['purple', 'magenta', 'violet', 'pink']
+  }
+
   constructor(props) {
     super(props)
     this.state = {
-      color: 'purple'
+      color: choice(this.props.allColors)
     }
     this.handleClick = this.handleClick.bind(this)
   }
 
-  handleClick() {
+  pickColor() {
+    let newColor = choice(this.props.allColors)
+    this.setState({ color: newColor })
+  }
 
+  handleClick() {
+    this.pickColor()
   }
 
   render() {
     return (
-      <div className='Box' style={{backgroundColor: this.state.color}} onClick={this.handleClick}>
-
+      <div
+        className='Box'
+        style={{ backgroundColor: this.state.color }}
+        onClick={this.handleClick}>
       </div>
     )
   }
